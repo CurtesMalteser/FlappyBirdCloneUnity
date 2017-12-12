0e46cfbb2147007ff6cbe286d48ef01d426a2764
CurtesMalteser <antonio_bastiao@hotmail.com>
2017-12-12 22:05:44 +0100

    Flappy Bird Clone on Unity - Official Turorial

---
+++ Flappy Bird Unity/Assets/Scripts/Bird.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour {
+
+	public float upForce = 200f; //Upward force of the "flap".
+	private bool isDead = false; //Has the player collided with a wall?
+
+	private Rigidbody2D rb2d; 
+	private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+
+		//Get and store a reference to the Rigidbody2D attached to this GameObject.
+		rb2d = GetComponent< Rigidbody2D > (); 
+		anim = GetComponent< Animator > ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Don't allow control if the bird has died.
+		if ( isDead == false ) 
+		{
+			
+			//Look for input to trigger a "flap".
+			if (Input.GetMouseButtonDown (0)) {
+				
+				//...tell the animator about it and then...
+
+
+
+				rb2d.velocity = Vector2.zero;
+				rb2d.AddForce (new Vector2 (0, upForce));
+				anim.SetTrigger ("Flap");
+			}
+
+			}
+		}
+
+		
+	void OnCollisionEnter2D () {
+
+		rb2d.velocity = Vector2.zero;
+		isDead = true;
+		anim.SetTrigger ("Die");
+		GameControl.instance.BirdDied ();
+
+	}
+
+}
---
+++ Flappy Bird Unity/Assets/Scripts/ScrollingObject.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingObject : MonoBehaviour {
+
+	private Rigidbody2D rg2d;
+
+	// Use this for initialization
+	void Start () {
+
+		rg2d = GetComponent< Rigidbody2D > ();
+		rg2d.velocity = new Vector2( GameControl.instance.scollSpeed, 0 );
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (GameControl.instance.gameOver == true) {
+		
+			rg2d.velocity = Vector2.zero;
+
+		}
+	}
+}
